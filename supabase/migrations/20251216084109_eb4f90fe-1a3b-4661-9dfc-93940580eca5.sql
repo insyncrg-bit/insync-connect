7cba970c1ca8839810ac14c81272ed0a6c5438a0
Shourya Yadav <shourya0523@gmail.com>
2026-02-08 00:02:22 -0500

    Cleanup

--- supabase/migrations/20251216084109_eb4f90fe-1a3b-4661-9dfc-93940580eca5.sql
+++
@@ -1,7 +0,0 @@
--- Add JSONB column to store the detailed investor-memo application data
-ALTER TABLE public.founder_applications 
-ADD COLUMN IF NOT EXISTS application_sections JSONB DEFAULT '{}'::jsonb;
-
--- Add team members column
-ALTER TABLE public.founder_applications 
-ADD COLUMN IF NOT EXISTS team_members JSONB DEFAULT '[]'::jsonb;
